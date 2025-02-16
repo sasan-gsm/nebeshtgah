932dfa20f9b1d5093d37c243b0aa5802892f4aa9
sassan <sasan.gsm@gmail.com>
2025-02-17 00:37:21 +0330

    profile model added

--- core_apps/profiles/models.py
+++ core_apps/profiles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+from core_apps.common.models import BaseTimeStampModel
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Profile(BaseTimeStampModel):
+    avatar = models.ImageField(
+        verbose_name=_("Profile Photo"), default="/profile_default.png"
+    )
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"), max_length=12, blank=True
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followers = models.ManyToManyField(
+        "self", symmetrical=False, related_name="following", blank=True
+    )
+
+    class Meta:
+        db_table = "profile"
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
