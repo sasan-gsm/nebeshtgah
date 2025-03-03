690b77b571c57fd6250da87963fd2da5ac22e2ad
sassan <s@s.com>
2025-03-03 22:11:38 +0330

    user urls modified

--- core_apps/users/urls.py
+++ core_apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PasswordResetView, PasswordResetConfirmView
+from .views import CustomPasswordResetView, CustomPasswordResetConfirmView
 
 urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
@@ -7,10 +7,10 @@ urlpatterns = [
         "auth/token/refresh/",
         include("rest_framework_simplejwt.views.TokenRefreshView"),
     ),
-    path("password/reset/", PasswordResetView.as_view(), name="password-reset"),
+    path("password/reset/", CustomPasswordResetView.as_view(), name="password-reset"),
     path(
         "password/reset/confirm/",
-        PasswordResetConfirmView.as_view(),
+        CustomPasswordResetConfirmView.as_view(),
         name="password-reset-confirm",
     ),
 ]
