6e3b39b8070a5dbf4867824decbe90542c1ab7ae
sassan <s@s.com>
2025-03-18 19:38:33 +0330

    user urls added.

--- core_apps/users/urls.py
+++ core_apps/users/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path, include
-from .views import CustomPasswordResetView, CustomPasswordResetConfirmView
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
 urlpatterns = [
-    path("auth/", include("dj_rest_auth.urls")),
-    path(
-        "auth/token/refresh/",
-        include("rest_framework_simplejwt.views.TokenRefreshView"),
-    ),
-    path("password/reset/", CustomPasswordResetView.as_view(), name="password-reset"),
-    path(
-        "password/reset/confirm/",
-        CustomPasswordResetConfirmView.as_view(),
-        name="password-reset-confirm",
-    ),
+    path("", include(router.urls)),
 ]
